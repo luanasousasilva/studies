2337b0af866159683b12d425f1cef640bba51580
Luana.silva <luana.silva@bitgov.com.br>
2024-10-07 11:58:04 -0300

    criando uma nota.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 
 
 function App() {
-  return (
-    <div className="App">
-      Hello Word
-    </div>
-  );
+    return (
+        <>
+            <form>
+                <input type="text" placeholder="Titulo" />
+                <textarea placeholder="Escreva sua nota..." />
+                <button>Criar nota</button>
+            </form>
+            <ul>
+                <li>
+                    <section>
+                        <header>
+                            <h3>Título</h3>
+                        </header>
+                        <p>Escreva sua nota</p>
+                    </section>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default App;
